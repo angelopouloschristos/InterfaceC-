a0683404a1735a5906a2cc50290bc3739a6910b0
Christos-Angelopoulos <cangelopoulos@helb-prigogine.be>
2022-03-26 16:52:56 +0100

    Intégration LOGIN
    Intégration LOGIN

--- CO2_Interface/Controls/AccountControl.Designer.cs
+++ CO2_Interface/Controls/AccountControl.Designer.cs
@@ -34,19 +34,21 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(316, 87);
+            this.label1.Location = new System.Drawing.Point(418, 46);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(46, 13);
+            this.label1.Size = new System.Drawing.Size(54, 16);
             this.label1.TabIndex = 0;
             this.label1.Text = "account";
             // 
             // AccountControl
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "AccountControl";
-            this.Size = new System.Drawing.Size(816, 285);
+            this.Size = new System.Drawing.Size(1088, 351);
             this.ResumeLayout(false);
             this.PerformLayout();
 
---
+++ CO2_Interface/WinForms/frmLogin.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace CO2_Interface.WinForms
+{
+    public partial class frmLogin : Form
+    {
+        public frmLogin()
+        {
+            InitializeComponent();
+        }
+
+        OleDbConnection con = new OleDbConnection("Provider=Microsoft.Jet.OLEDB.4.0;Data Source=db_users.mdb");
+        OleDbCommand cmd = new OleDbCommand();
+        OleDbDataAdapter da = new OleDbDataAdapter();
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            string login = "SELECT * FROM tbl_users WHERE username= '" + txtUsername.Text + "' and password= '" + txtpassword.Text + "'";
+            cmd = new OleDbCommand(login, con);
+            OleDbDataReader dr = cmd.ExecuteReader();
+
+            if (dr.Read() == true)
+            {
+                //The Form which will appear after loggin in
+                new MainForm().Show();
+                //ICI METTRE NOTRE FORM MAIN
+                this.Hide();
+            }
+            else
+            {
+                MessageBox.Show("Invalid Username or Password, Please Try Again", "Login Failed", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txtUsername.Text = "";
+                txtpassword.Text = "";
+                txtUsername.Focus();
+            }
+        }
+
+        private void checkbxShowPas_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkbxShowPas.Checked)
+            {
+                textBox1.PasswordChar = '\0';
+
+            }
+            else
+            {
+                textBox1.PasswordChar = '•';
+
+            }
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+            new frmRegister().Show();
+            this.Hide();
+        }
+    }
+}
